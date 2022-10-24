8dcf2df7121975bddacc6831c973fe0214aa6f91
Oshadhi Pabasara Kumari Ekanayaka <98898738+Oshadhi2github@users.noreply.github.com>
2022-10-24 21:20:02 +0530

    27 commit

--- main.js
+++ main.js
@@ -176,7 +176,7 @@ setResult=()=>{
     <td>${tempDataSet.question}</td> 
     <td>${tempDataSet.request_Answer}</td>
     <td>${tempDataSet.answer}</td>
-    <td>${tempDataSet.state}</td> 
+    <td>${tempDataSet.state?'😁':'😢'}</td> 
     <td>${tempDataSet.time}</td>
  </tr>`
         element.innerHTML+=newRow;
